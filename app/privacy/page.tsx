33013e4f6d26f4b7811a031cd48385167c196da4
berserker-glitch <hitlerachoja@gmail.com>
2026-01-04 22:01:52 +0100

    fixed the jsx error

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { JSX, useEffect, useState } from "react";
 import Link from "next/link";
 import { Header } from "@/components/landing/header";
 import { Footer } from "@/components/landing/footer";
