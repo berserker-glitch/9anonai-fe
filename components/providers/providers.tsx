d295f87771dbc77e16b8bf94264f4f5a83d8c86f
berserker-glitch <hitlerachoja@gmail.com>
2026-04-22 09:00:14 +0100

    feat: add Providers component to wrap application with necessary context providers

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ThemeProvider } from "./theme-provider";
 import { AuthProvider } from "@/lib/auth-context";
 import { LanguageProvider } from "@/lib/language-context";
@@ -14,17 +15,19 @@ interface ProvidersProps {
 // @ts-ignore
 export function Providers({ children, lang }: ProvidersProps & { lang?: string }) {
     return (
-        <AuthProvider>
-            <LanguageProvider defaultNamespaces={["landing", "tos", "privacy", "auth"]} initialLanguage={lang as any}>
-                <ThemeProvider defaultTheme="dark">
-                    <ToastProvider>
-                        <SmoothScrollProvider>
-                            {children}
-                        </SmoothScrollProvider>
-                    </ToastProvider>
-                </ThemeProvider>
-            </LanguageProvider>
-        </AuthProvider>
+        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+            <AuthProvider>
+                <LanguageProvider defaultNamespaces={["landing", "tos", "privacy", "auth"]} initialLanguage={lang as any}>
+                    <ThemeProvider defaultTheme="dark">
+                        <ToastProvider>
+                            <SmoothScrollProvider>
+                                {children}
+                            </SmoothScrollProvider>
+                        </ToastProvider>
+                    </ThemeProvider>
+                </LanguageProvider>
+            </AuthProvider>
+        </GoogleOAuthProvider>
     );
 }
 
